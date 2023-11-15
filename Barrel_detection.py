d7311fb7a4a6c4dbd347b2128131e9831071c5d9
BaudFrag <109072703+BaudFrag@users.noreply.github.com>
2023-11-15 15:33:15 +0000

    Add files via upload

--- Barrel_detection.py
+++ Barrel_detection.py
@@ -1,110 +1,133 @@
 import cv2
 import numpy as np
 
-# Load an image
-video = cv2.VideoCapture(0)  # Replace 'your_image.jpg' with your image
+class BarrelDetection:
+    def __init__(self):
+        self.flag = 1
+        self.center_coordinates = [480/2,640/2]
+        # Load an image
+        self.video = cv2.VideoCapture(0)
 
-# Define the HSV range for red color
-lower_red = np.array([0, 100, 100])  # Lower range for red in HSV
-upper_red = np.array([10, 255, 255])  # Upper range for red in HSV
+        # Define the HSV range for red color
+        self.lower_red = np.array([0, 100, 100])  # Lower range for red in HSV
+        self.upper_red = np.array([10, 255, 255])  # Upper range for red in HSV
 
-# Define the HSV range for green color
-lower_green = np.array([35, 100, 100])  # Lower range for green in HSV
-upper_green = np.array([85, 255, 255])  # Upper range for green in HSV
+        # Define the HSV range for green color
+        self.lower_green = np.array([35, 100, 100])  # Lower range for green in HSV
+        self.upper_green = np.array([85, 255, 255])  # Upper range for green in HSV
 
-def calibration(hsv_image, hue_range):
-    h, w, c = hsv_image.shape
-    x = int(w/2)
-    y = int(h/2)
-    hue, saturation, value = hsv_image[y][x]
+        print("Barrel detection class initialised")
 
-    hue_lower = hue-hue_range
-    if hue_lower < 0:
-        hue_lower = 0
-    saturation_lower = saturation-50
-    if saturation_lower < 0:
-        saturation_lower = 0
-    value_lower = value-50
-    if value_lower < 0:
-        value_lower = 0
+    def calibration(self, hsv_image, hue_range):
+        h, w, c = hsv_image.shape
+        x = int(w/2)
+        y = int(h/2)
+        hue, saturation, value = hsv_image[y][x]
 
-    lower = np.array([hue_lower, saturation_lower, value_lower])
+        hue_lower = hue-hue_range
+        if hue_lower < 0:
+            hue_lower = 0
+        saturation_lower = saturation-50
+        if saturation_lower < 0:
+            saturation_lower = 0
+        value_lower = value-50
+        if value_lower < 0:
+            value_lower = 0
 
-    hue_upper = hue+hue_range
-    if hue_upper > 360:
-        hue_upper = 360
-    saturation_upper = saturation+50
-    if saturation_upper>255:
-        saturation_upper = 255
-    value_upper = value+50
-    if value_upper > 255:
-        value_upper = 255
+        lower = np.array([hue_lower, saturation_lower, value_lower])
 
-    upper = np.array([hue_upper, saturation_upper, value_upper])
+        hue_upper = hue+hue_range
+        if hue_upper > 360:
+            hue_upper = 360
+        saturation_upper = saturation+50
+        if saturation_upper>255:
+            saturation_upper = 255
+        value_upper = value+50
+        if value_upper > 255:
+            value_upper = 255
 
-    return lower, upper
+        upper = np.array([hue_upper, saturation_upper, value_upper])
 
-while True:
-    ret, image = video.read()
+        return lower, upper
 
-    if not ret:
-        break
+    def get_center_coordinates(self):
+        ret, image = self.video.read()
 
-    # Convert the image to the HSV color space
-    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        if not ret:
+            self.flag = 0
 
-    # Create a mask to isolate green regions
-    green_mask = cv2.inRange(hsv_image, lower_green, upper_green)
+        # Convert the image to the HSV color space
+        hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-    # Create a mask to isolate red regions
-    red_mask = cv2.inRange(hsv_image, lower_red, upper_red)
+        # Create a mask to isolate green regions
+        green_mask = cv2.inRange(hsv_image, self.lower_green, self.upper_green)
 
-    mask = red_mask + green_mask
+        # Create a mask to isolate red regions
+        red_mask = cv2.inRange(hsv_image, self.lower_red, self.upper_red)
 
-    # Find contours in the green mask
-    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(image, contours, -1, (0,255,255)) #draw contours on image
+        mask = red_mask + green_mask
 
-    # Filter contours based on shape (assumes a cube)
-    detected_cubes=[]
-    min_cube_area = 500
-    for contour in contours:
-        area = cv2.contourArea(contour)
+        # Find contours in the green mask
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(image, contours, -1, (0, 255, 255))  # draw contours on image
 
-        if area >= min_cube_area:
-            # Approximate the contour to reduce points
-            epsilon = 0.04 * cv2.arcLength(contour, True)
-            approx = cv2.approxPolyDP(contour, epsilon, True)
+        # Filter contours based on shape (assumes a cube)
+        detected_cubes = []
+        min_cube_area = areaTmp = 500
+        largest_contour = []
+        center_coordinates = []
 
-            # Check if the shape is approximately a cube
-            if len(approx) == 4:
-                detected_cubes.append(approx)
+        # Calculate and store the areas of each contour in the dictionary
+        for idx, contour in enumerate(contours):
+            area = cv2.contourArea(contour)
 
-    # Draw rectangles around detected cubes
-    for cube in detected_cubes:
-        x, y, w, h = cv2.boundingRect(cube)
-        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            if area >= min_cube_area:
+                # Approximate the contour to reduce points
+                epsilon = 0.04 * cv2.arcLength(contour, True)
+                approx = cv2.approxPolyDP(contour, epsilon, True)
 
-        #Coordinate of the center
-        x_center = int(x + w/2)
-        y_center = int(y + h/2)
-        cv2.circle(image, (x_center, y_center), 3, (255,0,0), 3)
+                # Check if the shape is approximately a cube
+                if len(approx) == 4:
+                    detected_cubes.append(approx)
+                    if area > areaTmp:
+                        areaTmp = area
+                        largest_contour = contour
 
-    # Show the image with rectangles around detected cubes
-    cv2.imshow('Green Cube Detection', image)
-    cv2.imshow('Green mask', mask)
+        # Draw coordinates of the center of the biggest object
+        if len(largest_contour) > 0:
+            x, y, w, h = cv2.boundingRect(largest_contour)
+            x_center = int(x + w / 2)
+            y_center = int(y + h / 2)
+            self.center_coordinates = [x_center, y_center]
+            cv2.circle(image, (x_center, y_center), 3, (255, 0, 0), 3)
 
-    if cv2.waitKey(1) & 0xFF == ord('r'):
-        lower_red, upper_red = calibration(hsv_image, 10)
+        # Draw rectangles around detected cubes
+        for cube in detected_cubes:
+            x, y, w, h = cv2.boundingRect(cube)
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-    if cv2.waitKey(1) & 0xFF == ord('g'):
-        lower_green, upper_green = calibration(hsv_image, 5)
+        # Show the image with rectangles around detected cubes
+        cv2.imshow('Object Cube Detection', image)
+        cv2.imshow('Green+Red mask', mask)
 
-    if cv2.waitKey(1) & 0xFF == 27:  # Press 'Esc' to exit
-        break
+        if cv2.waitKey(1) & 0xFF == ord('r'):
+            self.lower_red, self.upper_red = self.calibration(hsv_image, 10)
+
+        if cv2.waitKey(1) & 0xFF == ord('g'):
+            self.lower_green, self.upper_green = self.calibration(hsv_image, 5)
+
+        if cv2.waitKey(1) & 0xFF == 27:  # Press 'Esc' to exit
+            self.flag=0
+
+        print(self.center_coordinates)
+
+if __name__ == '__main__':
+    myBarrelDetection = BarrelDetection()
+while myBarrelDetection.flag == 1:
+    myBarrelDetection.get_center_coordinates()
 
 # Release the VideoCapture and close all OpenCV windows
-video.release()
+myBarrelDetection.video.release()
 cv2.destroyAllWindows()
 
 
